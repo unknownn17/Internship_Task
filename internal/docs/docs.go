c601772cf46669219e0683cc541030d7b7bbfd98
Your Name <dostonxoshimov2005@gmail.com>
2024-09-07 00:37:50 +0500

    host adjusted for swagger

--- internal/docs/docs.go
+++ internal/docs/docs.go
@@ -479,7 +479,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "localhost:8080",
+	Host:             "3.127.221.197:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Task Management API",
